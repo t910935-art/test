31229e022fa12de749de4c42b264eef2e2f32754
t910935-art <t910935@gmail.com>
2026-09-06 07:23:42 +0200

    pc portable

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-
+// pc portable
 export function getRouter() {
   const router = createTanStackRouter({
     routeTree,
